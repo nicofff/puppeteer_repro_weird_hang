5436241409dc745ef63a815073df741c4871e677
Nicolas Far <nicolas.far@renaissance.com>
2026-04-05 10:32:36 -0400

    Clean up even more

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 ```bash
 docker run --platform linux/amd64 -i --init --rm ghcr.io/puppeteer/puppeteer:latest node -e "$(cat index.mjs)"
 ```
-
-It's not 100% consistent, you might need to run it multiple times.
--- index.mjs
+++ index.mjs
@@ -1,22 +1,29 @@
 import puppeteer from 'puppeteer';
 
+
 const browser = await puppeteer.launch({
   protocolTimeout: 20_000,
-  args: ['--no-sandbox', '--disable-gpu'], // Still fails if you comment this out and run with --cap-add=SYS_ADMIN
+  args: ['--no-sandbox'], // Still fails if you comment this out and run with --cap-add=SYS_ADMIN
 });
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 async function screenshotTask(id) {
-  await sleep(Math.random() * 3000);
+  await sleep(Math.random() * 4000);
+  console.log("Browser version", await browser.version())
   const page = await browser.newPage();
   await page.setViewport({ width: 1024, height: 768 });
-  await page.goto('https://cf.nearpod.com/neareducation/new/Scrubber/en/dash-slideEditor.png', { waitUntil: 'networkidle2'  });
+  // await page.goto('https://cf.nearpod.com/neareducation/new/Scrubber/en/dash-slideEditor.png', { waitUntil: 'networkidle2' });
+  console.log(`[tab${id}] goTo`);
+  await page.goto('https://cnn.com', { waitUntil: 'networkidle2'  });
   console.log(`[tab${id}] taking screenshot`);
+  //await page.bringToFront();
   await page.screenshot();
   console.log(`[tab${id}] done`);
   await page.close();
 }
 
-await Promise.all(Array.from({ length: 5 }, (_, i) => screenshotTask(i)));
+while (true) { 
+  await Promise.all(Array.from({ length: 5 }, (_, i) => screenshotTask(i)));
+}
 await browser.close();
